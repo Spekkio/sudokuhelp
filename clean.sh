a75ce0f953fb324b402bc31b48fa5aa74c1cf739
spekkio <maxifoo@gmail.com>
2014-09-05 20:51:51 +0200

    Installing doxygen support.

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -rf INSTALL Makefile.in aclocal.m4 autom4te.cache/ config.h.in configure depcomp install-sh missing Makefile config.h config.log config.status main src/.deps/ src/.dirstamp stamp-h1 *~ \#*\# *.o mdate-sh texinfo.tex doc/*.info doc/stamp-vti doc/version.texi config.guess config.sub ltmain.sh m4/* compile doc/
+
+find . -regex "^.*\.o$" -delete
+find . -regex "^.*~$" -delete
+#find . -regex "^.*\.deps$" -delete
+#find . -regex "^.*\.dirstamp$" -delete
