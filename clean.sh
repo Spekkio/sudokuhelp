d1df50ca08023bba3f91e1e64e636f4d717bb837
spekkio <maxifoo@gmail.com>
2014-09-06 15:56:51 +0200

    Moving doxygen m4 script to a better location.

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf INSTALL Makefile.in aclocal.m4 autom4te.cache/ config.h.in configure depcomp install-sh missing Makefile config.h config.log config.status main src/.deps/ src/.dirstamp stamp-h1 *~ \#*\# *.o mdate-sh texinfo.tex doc/*.info doc/stamp-vti doc/version.texi config.guess config.sub ltmain.sh m4/* compile doc/
+rm -rf INSTALL Makefile.in aclocal.m4 autom4te.cache/ config.h.in configure depcomp install-sh missing Makefile config.h config.log config.status main src/.deps/ src/.dirstamp stamp-h1 *~ \#*\# *.o mdate-sh texinfo.tex doc/*.info doc/stamp-vti doc/version.texi config.guess config.sub ltmain.sh compile doc/
 
 find . -regex "^.*\.o$" -delete
 find . -regex "^.*~$" -delete
